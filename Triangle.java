ebf9d003696f2cbb693f2e035f8cb300ac08a178
danielleep <113351600+danielleep@users.noreply.github.com>
2023-12-25 20:42:07 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -4,7 +4,12 @@
  * Write a program that tests if three given integers form a triangle.
  */ 
 public class Triangle {
-	public static void main(String[] args) {
-		// Put your code here	
+	public static void main(String[] args) 
+	{
+	int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        int z = Integer.parseInt(args[2]);
+        boolean form = ((x+y)>z) && ((y + z) > x)&&((z + x) > y);
+        System.out.println(x+ ", " + y + ", " + z + ": "+form);
 	}
 }
